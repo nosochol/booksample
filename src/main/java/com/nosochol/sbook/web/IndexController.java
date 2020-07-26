55f4be3012879cf0b4f18c679a0f9b77de5cd367
rhoseungchol <nosochol@naver.com>
2020-07-26 15:47:22 +0900

    test

--- src/main/java/com/nosochol/sbook/web/IndexController.java
+++ src/main/java/com/nosochol/sbook/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.nosochol.sbook.web;
 
+//import com.nosochol.sbook.config.auth.LoginUser;
+import com.nosochol.sbook.config.auth.dto.SessionUser;
 import com.nosochol.sbook.service.PostsService;
 import com.nosochol.sbook.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpServletRequest httpServlet;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpServlet.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
