a5d12181aade1fe2a925b41837c95cf49bf5e177
rhoseungchol <nosochol@naver.com>
2020-08-23 15:24:25 +0900

    test

--- src/main/java/com/nosochol/sbook/web/IndexController.java
+++ src/main/java/com/nosochol/sbook/web/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
